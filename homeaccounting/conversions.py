cb25d94a7548ca321f85e6e10d1e61c7c72d7b3a
Sebastian Bitzer <official@sbitzer.eu>
2017-01-15 22:47:42 +0100

    comprehensive changes to make conversions fail gracefully
    The main idea is to let the 'convert'-function return nan whenever it cannot
    convert between the desired symbols for whatever reason. This is to allow depot
    overviews to be calculated without errors.
    
    I catch all errors during conversion with external packages and pass them on as
    warnings. Warnings will also be given when symbols are used for which no
    conversion is implemented, yet.

--- homeaccounting/conversions.py
+++ homeaccounting/conversions.py
@@ -14,61 +14,103 @@ import numpy as np
 import re
 from warnings import warn
 
-symbol_re = re.compile(r'([A-Z]+)\.?([A-Z]+)?')
+symbol_re = re.compile(r'([A-Z\d]+)\.?([A-Z]+)?')
+common_currencies = ['EUR', 'USD', 'GBP', 'CAD', 'CNY', 'CHF', 'DKK', 'INR',
+                     'NOK', 'RUB', 'JPY', 'BRL', 'KRW']
 
-currency_map = {'DE': 'EUR'}
+currency_map = {'DE': 'EUR', None: 'USD'}
 
 def convert(x, from_symbol, to_symbol):
     from_symbol = from_symbol.upper()
     to_symbol = to_symbol.upper()
     
-    from_match = symbol_re.match(from_symbol)
-    to_match = symbol_re.match(to_symbol)
+    from_type = identify_symbol(from_symbol)
+    to_type = identify_symbol(to_symbol)
     
     y = None
     if from_symbol == to_symbol:
-        y = x
-    elif from_match is None:
+        return x
+    
+    if from_type is None:
         warn('converter: did not recognise from_symbol "%s", returning NaN!' % 
              from_symbol)
         return np.nan
-    elif to_match is None:
+    elif from_type == 'ISIN' or to_type == 'ISIN':
+        warn('converter: cannot currently convert assets to or from ISIN, '
+             'returning NaN!')
+        return np.nan
+    elif to_type is None:
         warn('converter: did not recognise to_symbol "%s", returning NaN!' % 
              to_symbol)
         return np.nan
-    elif to_match.groups()[1] is not None:
-        raise ValueError('Conversion to stocks currently not supported!')
     else:
-        from_groups = from_match.groups()
-        
-        # from bitcoin to currency
-        if from_groups[0] in ['BTC', 'XBT']:
-            bconv = fx.bitcoin.BtcConverter()
-            y = bconv.convert_btc_to_cur(x, to_symbol)
-        
-        # from something to bitcoin
-        elif to_symbol in ['BTC', 'XBT']:
-            if from_groups[1] is not None:
-                x = convert(x, from_symbol, 'EUR')
-                from_symbol = 'EUR'
+        try:
+            # from bitcoin to something
+            if from_type == 'bitcoin':
+                bconv = fx.bitcoin.BtcConverter()
+                if to_type != 'currency':
+                    x = bconv.convert_btc_to_cur(x, 'EUR')
+                    y = convert(x, 'EUR', to_symbol)
+                else:
+                    y = bconv.convert_btc_to_cur(x, to_symbol)
                 
-            bconv = fx.bitcoin.BtcConverter()
-            y = bconv.convert_to_btc(x, from_symbol)
-        
-        # from stock to something
-        elif from_groups[1] is not None:
-            stock_info = ystockquote.get_all(from_symbol)
-            price = convert(float(stock_info['price']), 
-                            currency_map[from_groups[1]], to_symbol)
-            y = x * price
-        
-        # from currency to currency
-        else:
-            cconv = fx.converter.CurrencyRates()
-            y = cconv.convert(from_symbol, to_symbol, x)
+            # from something to bitcoin
+            elif to_type == 'bitcoin':
+                if from_type != 'currency':
+                    x = convert(x, from_symbol, 'EUR')
+                    from_symbol = 'EUR'
+                    
+                bconv = fx.bitcoin.BtcConverter()
+                y = bconv.convert_to_btc(x, from_symbol)
+            
+            # from stock/fund to something
+            elif from_type == 'yahoo':
+                from_groups = symbol_re.match(from_symbol).groups()
+                stock_info = ystockquote.get_all(from_symbol)
+                price = convert(float(stock_info['price']), 
+                                currency_map[from_groups[1]], to_symbol)
+                y = x * price
+                
+            # from something to stock/fund
+            elif to_type == 'yahoo':
+                to_groups = symbol_re.match(to_symbol).groups()
+                stock_info = ystockquote.get_all(to_symbol)
+                price = convert(float(stock_info['price']), 
+                                currency_map[to_groups[1]], from_symbol)
+                y = x / price
+            
+            # from currency to currency
+            elif from_type == 'currency' and to_type == 'currency':
+                cconv = fx.converter.CurrencyRates()
+                y = cconv.convert(from_symbol, to_symbol, x)
+                
+        except Exception as err:
+            warn('converter: unexpected error ({}) during conversion, '
+                 'returning NaN!'.format(err))
+            return np.nan
             
     if y is None:
         y = np.nan
         warn('converter: did not recognise one of the symbols, returning NaN!')
         
     return y
+    
+    
+def identify_symbol(symbol):
+    match = symbol_re.match(symbol)
+    
+    if match is None:
+        return None
+    
+    groups = match.groups()
+        
+    if groups[1] is not None:
+        return 'yahoo'
+    elif len(groups[0]) == 12:
+        return 'ISIN'
+    elif groups[0] in common_currencies:
+        return 'currency'
+    elif groups[0] in ['BTC', 'XBT']:
+        return 'bitcoin'
+    else:
+        return 'yahoo'
